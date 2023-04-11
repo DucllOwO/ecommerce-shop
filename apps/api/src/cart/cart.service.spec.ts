716ee4380972317a8653ea7f9fea6634738ee1e7
Kim Dien <20520442@gm.uit.edu.vn>
2023-04-11 13:41:11 +0700

    fix db

--- apps/api/src/cart/cart.service.spec.ts
+++ apps/api/src/cart/cart.service.spec.ts
@@ -15,4 +15,13 @@ describe('CartService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('get cart should have returned', () => {
+    expect(service.cart({id: 1})).toHaveReturned();
+  })
+
+  it('post cart should have returned', () => {
+    expect(service.createCart({owner: {connect: {id: '079202011909'}}})).toHaveReturned();
+  })
+  
 });
