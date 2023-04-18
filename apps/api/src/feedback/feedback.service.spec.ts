36de73ab024611ffa81800f9e13a209684bc48cd
Kim Dien <20520442@gm.uit.edu.vn>
2023-04-18 13:35:13 +0700

    feedback service unit test

--- apps/api/src/feedback/feedback.service.spec.ts
+++ apps/api/src/feedback/feedback.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FeedbackService } from './feedback.service';
+import exp from 'constants';
 
 describe('FeedbackService', () => {
   let service: FeedbackService;
@@ -15,4 +16,35 @@ describe('FeedbackService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('get feedback should have returned', () => {
+    expect(service.feedback({id: 1})).toHaveReturned();
+  })
+
+  it('get all feedback should have returned', () => {
+    expect(service.feedbacks({})).toHaveReturned();
+  })
+
+  it('create feedback should have returned', () => {
+    expect(service.createFeedback({
+      content: "test",
+      rate: 5,
+      author: {
+        connect: {
+          id: '079202011909'
+        }
+      }
+    })).toHaveReturned();
+
+    it('update feedback should have returned', () => {
+      expect(service.updateFeedback({
+        where: {
+          id: 1
+        },
+        data: {
+          rate: 4
+        },
+      })).toHaveReturned();
+    })
+  })
 });
