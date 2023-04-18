672c038466cb941440d9604db33cd6f2beeacc60
Kim Dien <20520442@gm.uit.edu.vn>
2023-04-18 14:20:44 +0700

    order service unit test

--- apps/api/src/order/order.service.spec.ts
+++ apps/api/src/order/order.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrderService } from './order.service';
+import exp from 'constants';
+import dayjs from 'dayjs';
 
 describe('OrderService', () => {
   let service: OrderService;
@@ -15,4 +17,35 @@ describe('OrderService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('get order with {id: 1} should have returned with', () => {
+    expect(service.order({id: 1})).toHaveReturned();
+  })
+
+  it('get all order should have returned', () => {
+    expect(service.orders({})).toHaveReturned();
+  })
+
+  it('create order should have returned with', () => {
+    expect(service.createOrder({
+      date: new Date(Date.now()),
+      buyer: {
+        connect: {
+          id: '079202011909'
+        }
+      },
+      total_cost: 1200000,
+      status: '0',
+      Order_detail: {
+        create: {
+          product_item: {
+            connect: {
+              id: 1
+            }
+          }
+        }
+      }
+
+    }))
+  })
 });
