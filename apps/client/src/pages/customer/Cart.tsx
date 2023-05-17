49d69e108ce4ca9a7dfcfcd8d07722618311488e
DucllOwO <nguyenduc147862@gmail.com>
2023-05-17 15:09:36 +0700

    fix ui cart

--- apps/client/src/pages/customer/Cart.tsx
+++ apps/client/src/pages/customer/Cart.tsx
@@ -1,5 +1,5 @@
 import { UserOutlined } from '@ant-design/icons'
-import { Avatar, Button, Col, Form, Input, Row, Space } from 'antd'
+import { Avatar, Button, Col, Form, Input, Row, Select, Space } from 'antd'
 import React, { useEffect, useState } from 'react'
 
 import { Link } from 'react-router-dom'
@@ -9,8 +9,13 @@ import CartItem, { CartItemType } from './components/CartItem'
 import Helmet from './components/Helmet'
 import CartTable from './components/table/CartTable'
 
-const Cart = () => {
+const paymentMethods = [
+    { value: 'cod', label: 'Thanh toán khi nhận hàng' },
+    { value: 'momo', label: 'Chuyển tiền qua MoMo' },
+    { value: 'bank', label: 'Chuyển tiền qua ngân hàng' },
+]
 
+const Cart = () => {
 
     const [cartProducts, setCartProducts] = useState(productData.getProducts(4))
 
@@ -28,35 +33,35 @@ const Cart = () => {
                     <Space direction='vertical' style={{ width: '90%' }}>
                         <Form layout='vertical' style={{ paddingTop: 20 }}>
                             <Form.Item
-                                label="Email"
+                                label='email'
                                 name="email"
                                 rules={[REQUIRED_RULE, EMAIL_FORMAT_RULE]}
                             >
-                                <Input />
+                                <Input placeholder='Email của bạn' />
                             </Form.Item>
                             <Form.Item
-                                label="First Name"
-                                name="firstName"
+                                label="Họ"
+                                name="lastname"
                                 rules={[REQUIRED_RULE]}
                             >
                                 <Input />
                             </Form.Item>
                             <Form.Item
-                                label="Last Name"
-                                name="lastName"
+                                label="Tên"
+                                name="firstname"
                                 rules={[REQUIRED_RULE]}
                             >
                                 <Input />
                             </Form.Item>
                             <Form.Item
-                                label="Address"
+                                label="Địa chỉ"
                                 name="address"
                                 rules={[REQUIRED_RULE]}
                             >
                                 <Input />
                             </Form.Item>
                             <Form.Item
-                                label="Phone Number"
+                                label="Số điện thoại"
                                 name="phoneNumber"
                                 rules={[REQUIRED_RULE, PHONENUMBER_FORMAT_RULE]}
                             >
@@ -68,13 +73,19 @@ const Cart = () => {
                             >
                                 <Input />
                             </Form.Item>
+                            <Form.Item
+                                label="Phương thức thanh toán"
+                                name="paymentMethod">
+                                <Select placeholder='Chọn phương thức thanh toán' options={paymentMethods} />
+                            </Form.Item>
                         </Form>
                         <div className="cart__info">
                             <div className="cart__info__txt">
                                 <p>
                                     Bạn đang có {totalProducts} sản phẩm trong giỏ hàng
                                 </p>
-                                <div className="cart__info__txt__price">
+                                <div
+                                    className="cart__info__txt__price">
                                     <span>Thành tiền:</span> <span>{100000000}</span>
                                 </div>
                             </div>
