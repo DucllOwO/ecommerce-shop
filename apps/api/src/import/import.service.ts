9c697d78d5bcb77fd51ba41ca8a89646a6ff9bd3
Kim Dien <20520442@gm.uit.edu.vn>
2023-06-15 15:29:15 +0700

    import route

---
+++ apps/api/src/import/import.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+import { Importing, Prisma } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class ImportService {
+  constructor(private prisma: PrismaService) { }
+
+  async importing(
+    importingWhereUniqueInput: Prisma.ImportingWhereUniqueInput,
+  ): Promise<Importing | null> {
+    return this.prisma.importing.findUnique({
+      where: importingWhereUniqueInput,
+    });
+  }
+
+  async importings(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.ImportingWhereUniqueInput;
+    where?: Prisma.ImportingWhereInput;
+    orderBy?: Prisma.ImportingOrderByWithRelationInput;
+  }): Promise<Importing[]> {
+    const { skip, take, orderBy, where } = params;
+    return this.prisma.importing.findMany({
+      where,
+      skip,
+      take,
+      orderBy,
+    });
+  }
+  async createImporting(data: Prisma.ImportingCreateInput): Promise<Importing> {
+    return this.prisma.importing.create({
+      data,
+    });
+  }
+
+//   async updateImporting(params: {
+//     where: Prisma.ImportingWhereUniqueInput,
+//     data: Prisma.ImportingCreateInput
+//   }): Promise<Importing> {
+//     const { where, data } = params
+//     return this.prisma.importing.update({
+//       where,
+//       data
+//     });
+//   }
+}
