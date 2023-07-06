5b6c0bcdb1df598f49e015e137bced4f59e55456
Kim Dien <20520442@gm.uit.edu.vn>
2023-07-06 16:13:57 +0700

    fix dashboard

--- apps/client/src/api/admin/dashboardAPI.ts
+++ apps/client/src/api/admin/dashboardAPI.ts
@@ -3,3 +3,11 @@ import { http } from "..";
 export function fetchReport() {
     return http.get(`/report/yearly`);
 }
+
+export function fetchTopTenBestSellers() {
+    return http.get(`/product/best-sellers`);
+}
+  
+export function fetchTopTenMostViewed() {
+    return http.get(`/product/most-viewed`);
+}
